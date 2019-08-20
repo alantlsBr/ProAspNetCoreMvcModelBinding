f1b78777885151f1cbdcec1da8ac86aab3072780
Alan Teles de Oliveira <alantls@hotmail.com.br>
2019-08-20 19:46:47 -0400

    Criado modelo de dados

---
+++ Models/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using ProAspNetCoreMvcModelBinding.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProAspNetCoreMvcModelBinding.Models
+{
+    public class Pessoa
+    {
+        public int PessoaId { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public bool Aprovado { get; set; }
+        public Permissao Permissao { get; set; }
+    }
+}
