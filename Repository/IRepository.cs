9884b2e53cbd0d8539921bde10e02bea565f20dd
Alan Teles de Oliveira <alantls@hotmail.com.br>
2019-08-20 20:01:43 -0400

    criado o repositorio memoryrepository

--- Repository/IRepository.cs
+++ Repository/IRepository.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using ProAspNetCoreMvcModelBinding.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ProAspNetCoreMvcModelBinding.Repository
 {
-    public class IRepository
+    public interface IRepository
     {
+        IEnumerable<Pessoa> Pessoa { get; }
+        Pessoa this[int id] { get; set; }
     }
 }
