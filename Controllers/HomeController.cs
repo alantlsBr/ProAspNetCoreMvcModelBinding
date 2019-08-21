bf660568a63533cf95594346932a2137ba39854f
Alan Teles de Oliveira <alantls@hotmail.com.br>
2019-08-20 20:07:41 -0400

    Alterado controle home adicionando o repositorio

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProAspNetCoreMvcModelBinding.Repository;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,6 +11,17 @@ namespace ProAspNetCoreMvcModelBinding.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index() => View();
+        private IRepository repository;
+
+        public HomeController(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public ViewResult Index(int id)
+        {
+            return View("Index", repository[id]);
+        }
+
     }
 }
